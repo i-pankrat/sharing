38ff448e08b4639756edd660f7b54f0920d9952d
Ilya Pankratov <i.pankratov.main@gmail.com>
2023-10-08 17:32:01 +0300

    Add D-Bus Introspection
    Signed-off-by: Ilya Pankratov <i.pankratov.main@gmail.com>

--- src/sharingdbusadaptor.h
+++ src/sharingdbusadaptor.h
@@ -10,6 +10,35 @@ class SharingDBusAdaptor : public QDBusAbstractAdaptor {
     Q_OBJECT;
 
     Q_CLASSINFO("D-Bus Interface", "ru.pankrat.Sharing");
+    Q_CLASSINFO("D-Bus Introspection",
+        ""
+        "  <interface name=\"ru.i-pankrat.Sharing\">\n"
+        "    <method name=\"registerApp\">\n"
+        "      <arg direction=\"in\" type=\"s\" name=\"appname\"/>\n"
+        "    </method>\n"
+        "    <method name=\"getRegisteredApps\">\n"
+        "      <arg direction=\"out\" type=\"as\"/>\n"
+        "    </method>\n"
+        "    <method name=\"addFileType\">\n"
+        "      <arg direction=\"in\" type=\"s\" name=\"appname\"/>\n"
+        "      <arg direction=\"in\" type=\"s\" name=\"filetype\"/>\n"
+        "    </method>\n"
+        "    <method name=\"getAvailableFileTypes\">\n"
+        "      <arg direction=\"in\" type=\"s\" name=\"appname\"/>\n"
+        "      <arg direction=\"out\" type=\"as\"/>\n"
+        "    </method>\n"
+        "    <method name=\"addOpenWay\">\n"
+        "      <arg direction=\"in\" type=\"s\" name=\"appname\"/>\n"
+        "      <arg direction=\"in\" type=\"s\" name=\"str\"/>\n"
+        "    </method>\n"
+        "    <method name=\"addOpenWay\">\n"
+        "      <arg direction=\"in\" type=\"s\" name=\"appname\"/>\n"
+        "      <arg direction=\"in\" type=\"s\" name=\"filetype\"/>\n"
+        "      <arg direction=\"in\" type=\"s\" name=\"file\"/>\n"
+        "      <arg direction=\"out\" type=\"s\"/>\n"
+        "    </method>\n"
+        "  </interface>\n"
+        "");
 
 public:
     explicit SharingDBusAdaptor(QObject *parent);
@@ -19,7 +48,7 @@ public Q_SLOTS:
     QStringList getRegisteredApps();
     void addFileType(QString appname, QString filetype);
     QStringList getAvailableFileTypes(QString appname);
-    void addOpenWay(QString appname, QString st);
+    void addOpenWay(QString appname, QString str);
     QString open(QString appname, QString filetype, QString file);
 
 Q_SIGNALS:
